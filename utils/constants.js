9a55259a9eedc71e09baa3b592d6d5e4a0866863
rsbassi9 <rajvir.bassi95@gmail.com>
2023-06-14 06:08:51 -0700

    Imported Wallets from Solana

--- utils/constants.js
+++ utils/constants.js
@@ -1,10 +1,11 @@
-import { Keypair, PublicKey } from "@solana/web3.js";
+//import { Keypair, PublicKey } from "@solana/web3.js";
+
+//export const RPC_ENDPOINT = "https://quiet-flashy-cherry.solana-devnet.discover.quiknode.pro/2f6b70a2c5f5be9a0e478c0857e4078d4cd9bde1/";
+//const keypair = Keypair.generate();
+//export const PROGRAM_ID = keypair.publicKey;
+
+import { PublicKey } from "@solana/web3.js";
 
 export const RPC_ENDPOINT = "https://quiet-flashy-cherry.solana-devnet.discover.quiknode.pro/2f6b70a2c5f5be9a0e478c0857e4078d4cd9bde1/";
 
-const keypair = Keypair.generate();
-
-export const PROGRAM_ID = keypair.publicKey;
-
-
-//console.log('Generated public key:', publicKey.toString());
+export const PROGRAM_ID = new PublicKey("5ExEq9ZxGfwSyw5ov4UFK7ws9NpDf2KnUk3wLqfAir4m")
