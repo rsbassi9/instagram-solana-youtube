8b044729baac7f034d51fb51b6f86e0c457338d5
rsbassi9 <rajvir.bassi95@gmail.com>
2023-06-14 06:47:18 -0700

    connected app to the endpoint and added documentation

--- pages/_app.js
+++ pages/_app.js
@@ -20,13 +20,45 @@ import "@solana/wallet-adapter-react-ui/styles.css"
 function MyApp({ Component, pageProps }) {
   const [mounted, setMounted] = useState(false);
 
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter()
+    ], []
+  );
+
   // In order to fix SSR error with Next
   useEffect(() => {
     setMounted(true);
   }, []);
 
   return (
-    <Component {...pageProps} />
+
+    // establish the connection to the blockchain network using the RPC_ENDPOINT.
+    // commitment level set to confirmed so the application will consider a transaction confirmed once it 
+    //is included in a block and a certain number of confirmations have been received
+    <ConnectionProvider
+      endpoint ={RPC_ENDPOINT}
+      config = {{commitment: "confirmed"}}
+    >
+      // WalletProvider manages the wallet connections fort the app.
+      // autoConnect allows the app to connect to a wallet if it already exists and is logged in
+      <WalletProvider wallets={wallets} autoConnect>
+        
+        // WalletModalProvider provides a modal(popup) for users to interact with their wallets
+        // The added condition then renders the amin component and passes down any page-specific properties (...pageProps)
+        // The application will only rendered once the modal variable is true to ensure the app is fully initialized before rendering the main component
+        <WalletModalProvider>
+          {
+            mounted && (
+              //<GlobalState>
+                <Component {...pageProps} />
+              //</GlobalState>
+            )
+          }
+        </WalletModalProvider>
+      </WalletProvider >
+
+    </ConnectionProvider>
   )
 }
 
