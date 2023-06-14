463a68340ce4dd03267e48ce2905228c768352ce
rsbassi9 <rajvir.bassi95@gmail.com>
2023-06-14 06:09:25 -0700

    Imported wallets from Solana into app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,20 @@ import '../styles/globals.css'
 import { useEffect, useMemo, useState } from "react";
 import { RPC_ENDPOINT } from "../utils";
 
-//Solana Imports
+//Wallet Imports
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import {
+  PhantomWalletAdapter
+} from "@solana/wallet-adapter-wallets"
+
+//Import button Styling from Solana
+import "@solana/wallet-adapter-react-ui/styles.css"
+ 
 
 function MyApp({ Component, pageProps }) {
   const [mounted, setMounted] = useState(false);
