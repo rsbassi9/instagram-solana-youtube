b19fb0746dec82ab54db3d738ec051df84c66a8c
rsbassi9 <rajvir.bassi95@gmail.com>
2023-06-14 05:27:35 -0700

    First commit

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,10 @@
-import { PublicKey } from "@solana/web3.js";
+import { Keypair, PublicKey } from "@solana/web3.js";
 
-export const RPC_ENDPOINT = "";
+export const RPC_ENDPOINT = "https://quiet-flashy-cherry.solana-devnet.discover.quiknode.pro/2f6b70a2c5f5be9a0e478c0857e4078d4cd9bde1/";
 
-export const PROGRAM_ID = new PublicKey("11111111111")
+const keypair = Keypair.generate();
+
+export const PROGRAM_ID = keypair.publicKey;
+
+
+//console.log('Generated public key:', publicKey.toString());
