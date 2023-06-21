6d9db923a81f5a2e7a665e59e36f7bdc138ef257
rsbassi9 <rajvir.bassi95@gmail.com>
2023-06-21 06:02:37 -0700

    user not found being logged to console. updated IDL, issue still persists

--- state/global.js
+++ state/global.js
@@ -1,8 +1,14 @@
 //This file allows us to use context to pull our components rather than app drilling
 
 import { createContext, useCallback, useEffect, useState } from "react";
-import { getProgram } from "../utils/program";
 import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+
+import { 
+    getProgram,
+    getPostAccountPk,
+    getLikeAccountPk,
+    getUserAccountPk, 
+} from "../utils";
 
 import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
@@ -25,6 +31,8 @@ export const GlobalContext = createContext({
 // set the state to program, so that it can be used anywhere once state is updated
 export const GlobalState = ({children}) => {
     const [program, setProgram] = useState()
+    const [isConnected, setIsConnected] = useState()
+    const [userAccount, setUserAccount] = useState()
 
     // get a connection and wallet
     const { connection } = useConnection()
@@ -32,17 +40,82 @@ export const GlobalState = ({children}) => {
 
     //set up useEffect to run on create, upload, or delete of a component 
     useEffect(()=> {
-
+      const fetchProgram = async () => {
         //check if theres a connection and wallet. if not, set the wallet to empty. if there is, set the program to whatever it is
         if(connection){
-            setProgram(getProgram(connection, wallet ?? {}))
+            setProgram(getProgram(connection, wallet ?? {}));
         } else {
-            setProgram(null)
+            setProgram(null);
         }
+      };
 
+      fetchProgram();
         //set our dependency module below to connection and wallet. useEffect will run when the page first loads, AND 
         //if get a connection and wallet OR if that changes at all
-    }, [connection, wallet])
+    }, [connection, wallet]);
+
+    // Check for a wallet connection
+    useEffect(() => {
+        setIsConnected(!!wallet?.publicKey); // will lead to true if there is a connecte wallet. flase otherwise
+    }, [wallet]);
+
+    const fetchUserAccount = useCallback(async () => {
+        if (!program || !wallet || !connection) return;
+      
+        try {
+          await connection.getAccountInfo(program.programId);
+      
+          const userAccountPk = await getUserAccountPk(wallet?.publicKey);
+          if (!userAccountPk) {
+            throw new Error("User account not found");
+          }
+          console.log(userAccountPk);
+      
+          const userAccount = await program.account.user.fetch(userAccountPk);
+          console.log("User found!");
+          setUserAccount(userAccount);
+        } catch (e) {
+          setUserAccount(null);
+          console.log("No user found!");
+        }
+      }, [program, wallet, connection]);
+      
+
+    // Check for a user account by fetching a user
+    /*const fetchUserAccount = useCallback( async () => {
+        
+        //if theres no program, return nothing
+        if(!program || !wallet || !connection) return;
+
+        //console.log("Program:", program);
+        //console.log("Wallet:", wallet);
+        //console.log("IsConnected:", isConnected);
+
+        // if there is a program, make sure the wallet has loaded (?) and then fetch its public key:
+        try {
+            await connection.getAccountInfo(program.programId);
+
+            console.log("Program:", program);
+            console.log("Wallet:", wallet);
+            console.log("IsConnected:", isConnected);
+
+            const userAccountPk = await getUserAccountPk(wallet?.publicKey)
+            console.log(userAccountPk)
+            const userAccount = await program.account.user.fetch(userAccountPk);
+            console.log("user found!")
+            setUserAccount(userAccount)
+
+        //otherwise, if there is a program and no user is found
+        } catch (e) {
+            setUserAccount(null);
+            console.log("Error fetching user account:", e);
+        }
+    }, [program,wallet,connection]);*/
+
+    // Check for user account
+    useEffect (() => {
+        fetchUserAccount();
+    }, [program, wallet, isConnected]);
 
     return(
         <GlobalContext.Provider
--- utils/program.js
+++ utils/program.js
@@ -4,13 +4,14 @@ import { PublicKey } from "@solana/web3.js";
 import { PROGRAM_ID } from "./constants";
 
 // This function lets us see into our Solana program and access the fucntions
-export const getProgram = (connection, wallet) => {
+export const getProgram = async (connection, wallet) => {
   const IDL = require("./idl.json");
   const provider = new AnchorProvider(
     connection,
     wallet,
     AnchorProvider.defaultOptions()
   );
+  await provider.ready;
   const program = new Program(IDL, PROGRAM_ID, provider);
   return program;
 };
@@ -31,6 +32,8 @@ export const getPostAccountPk = async (owner, id) => {
       [
         Buffer.from("post"),
         owner.toBuffer(),
+
+        // change our input type to something Solana can read
         new BN(id).toArrayLike(Buffer, "le", 8),
       ],
       PROGRAM_ID
@@ -42,9 +45,15 @@ export const getLikeAccountPk = async (owner, id, liker) => {
   return (
     await PublicKey.findProgramAddressSync(
       [
+        // change format of a string to something Solana can read from our seed
         Buffer.from("like"),
+        
+        // change format of a Public Key to something Solana can read from our seed
         owner.toBuffer(),
+        
+        // change format of a u64 to something Solana can read from our seed
         new BN(id).toArrayLike(Buffer, "le", 8),
+        
         liker.toBuffer(),
       ],
       PROGRAM_ID
